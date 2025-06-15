9226d6b67acbdd7f0bd1e6d6cfd41c3c041b3371
Ali Zakeri <zakeriali1zp@gmail.com>
2025-06-14 21:04:59 -0400

    Scanner changes

---
+++ vulnScanner/scanner.py
@@ -0,0 +1,48 @@
+import socket
+
+
+#define a function called scan_ports that takes in two parameters
+def scan_ports(host, ports):
+
+#initialize an empty list called open_ports
+    open_ports = []
+#start a for loop that goes over every port in ports
+    for port in ports:
+        try:
+
+'''create a socket object from the socket module
+socket.AF_INET specifies the address family as IPV4, meaning we'll only use IPv4 addresses
+socket.SOCK_STREAM specifies the socket type as TCP, this is necessary to scan services like HTTPS or SSH
+for UDP use : SOCK_DGRAM
+a new socket object is created for each port
+'''
+ 	    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+#set a response expectancy of 0.5 seconds before moving on to the next port 
+           sock.settimeout(0.5)
+#store the result after attempting a TCP connection
+            result = sock.connect_ex((host, port))
+#if the result is 0, then the connection attempt was sucessful aka port is open.
+#indicates that a service is listening on that port
+            if result == 0:
+#add all open ports to the open_ports liste we declared earlier
+                open_ports.append(port)
+            sock.close()
+#invalid hostname exception
+        except socket.gaierror as e:
+            print(f"Error scanning port {port}: Hostname resolution failed - {e}")
+#connection took too long
+	except socket.timout as e:
+	    print(f"Error scanning port {port}: connection timed out - {e}")
+#permission issues or other problems
+	except OSerror as e:
+	    print(f"Error scanning port {port}: Other issues prevailed - {e}")
+
+if open_ports:
+	return "\n".join(f"port {port} is open" for port in open_ports)
+return "no ports open" 
+
+
+
+    return open_ports
+
+
